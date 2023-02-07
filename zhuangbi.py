b36a9f69cc1582fadfb5f6be9fec9e4afeda8172
NNNannX <yh22540@bristol.ac.uk>
2023-02-07 06:56:12 +0000

    1

---
+++ zhuangbi.py
@@ -0,0 +1,56 @@
+int main() {
+    char answer[10];
+    printf("你\n是\n");
+    printf("有趣的灵魂\n我喜欢你，答应我吧！\n输入520\n");
+    scanf("%s", answer);
+    char b[4] = "520";
+    if (strcmp(answer, b) == 0) {
+        float y, x, z, f;
+        for (y = 1.5f; y > -1.5f; y -= 0.1f) {
+            for (x = -1.5f; x < 1.5f; x += 0.05f) {
+                z = x * x + y * y - 1;
+                f = z * z * z - x * x * y * y * y;
+                putchar(f <= 0.0f ? "*********"[(int)(f * -8.0f)] : ' ');
+            }
+            putchar('\n');
+        }
+        long time;
+        for (;;) {
+            system("color a");
+            for (time = 0; time < 99999999; time++);
+            system("color b");
+            for (time = 0; time < 99999999; time++);
+            system("color c");
+            for (time = 0; time < 99999999; time++);
+            system("color d");
+            for (time = 0; time < 99999999; time++);
+            system("color e");
+            for (time = 0; time < 99999999; time++);
+            system("color f");
+            for (time = 0; time < 99999999; time++);
+            system("color 0");
+            for (time = 0; time < 99999999; time++);
+            system("color 1");
+            for (time = 0; time < 99999999; time++);
+            system("color 2");
+            for (time = 0; time < 99999999; time++);
+            system("color 3");
+            for (time = 0; time < 99999999; time++);
+            system("color 4");
+            for (time = 0; time < 99999999; time++);
+            system("color 5");
+            for (time = 0; time < 99999999; time++);
+            system("color 6");
+            for (time = 0; time < 99999999; time++);
+            system("color 7");
+            for (time = 0; time < 99999999; time++);
+            system("color 8");
+            for (time = 0; time < 99999999; time++);
+            system("color 9");
+        }
+        getchar();
+
+    } else {
+        printf("\n好吧！\n");
+    }
+    return
